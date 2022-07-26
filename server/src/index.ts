83478da10b622a56f949de8fe81d51d8bc0aeb8b
diyor <sabirovdiyorkind@gmail.com>
2022-07-26 19:36:08 +0500

    underlying implementation:TEST!

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,3 +1,30 @@
-import { AuthUseCase } from './domain/usecase/auth.usecase';
+import net from 'net';
 
-console.log(AuthUseCase);
+const arrSocket: Array<net.Socket> = [];
+const chatHistory: string[] = [];
+const onData = (data: string) => {
+  chatHistory.push(data.toString());
+  arrSocket.forEach((socket) => {
+    socket.write(data);
+  });
+};
+
+const server = net
+  .createServer((socket) => {
+    socket.setNoDelay(true);
+    socket.write(chatHistory.toString());
+    arrSocket.push(socket);
+
+    socket.on('data', onData);
+
+    socket.on('error', (error) => {
+      console.log(error);
+    });
+  })
+  .listen(
+    {
+      host: 'localhost',
+      port: 2000,
+    },
+    () => console.log('\x1b[37m%s\x1b[0m', 'server start !')
+  );
