c13e66fefb49f6f2a94702a77c495afb006c9b68
diyor <sabirovdiyorkind@gmail.com>
2022-08-02 21:27:59 +0500

    data collection implementation

--- client/index.js
+++ client/index.js
@@ -1,32 +1,55 @@
 const net = require('net');
-const { Buffer } = require('buffer');
-
+const readline = require('readline');
+const { stdin: input, stdout: output } = require('process');
+const rl = readline.createInterface({ input, output });
+const questions = require('./question.js');
 const socket = new net.Socket();
 
+const createQuestion = (theQuestion) => {
+  return new Promise((resolve, reject) => {
+    try {
+      rl.question(theQuestion + ': ', (theAnswer) => resolve(theAnswer));
+    } catch (err) {
+      reject(err);
+    }
+  });
+};
+const startQuestions = (async () => {
+  for (let [
+    _,
+    { question: questionText, fn: responseFunction },
+  ] of Object.entries(questions)) {
+    const answer = await createQuestion(questionText);
+    responseFunction(answer);
+  }
+  rl.close();
+})();
+
+const connecttoServer = () => {
+  socket.connect(
+    {
+      port: 2000,
+      host: 'localhost',
+    },
+    () => {
+      console.log('\x1b[46m%s\x1b[0m', 'LOG', 'Сlient connect to server 🔛');
+      socket.write(
+        JSON.stringify({ summon: 'all', messagae: 'hello server from client' }),
+        'utf8'
+      );
+    }
+  );
+};
+rl.on('close', connecttoServer);
 socket.on('data', (data) => {
   console.log('server->' + data.toString().split(','));
 });
-
-socket.connect(
-  {
-    port: 2000,
-    host: 'localhost',
-  },
-  () => {
-    console.log('\x1b[46m%s\x1b[0m', 'LOG', 'Сlient connect to server 🔛');
-
-    socket.write(
-      JSON.stringify({ summon: 'all', messagae: 'hello server from client' }),
-      'utf8'
-    );
-  }
-);
-
 socket.on('error', (error) =>
   console.log('\x1b[41m%s\x1b[0m', 'ERROR', { ...error })
 );
 
-process.on('SIGINT', () => {
-  socket.end();
-  process.exit(1);
-});
+process.on('SIGINT', () => socket.end(() => process.exit(1)));
+
+for (let i = 0; i < 10000; i++) {
+  console.log('');
+}
