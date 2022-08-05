4e7b82b9aafd8b58c034ae144cfb8e160a5a931b
diyor <sabirovdiyorkind@gmail.com>
2022-08-05 12:42:57 +0500

    wrote a test for the correct data were saved during the registration process

--- server/src/domain/__tests__/auth.test.ts
+++ server/src/domain/__tests__/auth.test.ts
@@ -1,9 +1,14 @@
 import { ErrorGenerator } from '../abstracts';
+import { UserEntity } from '../entities/auth';
+import { AuthGatewayInterface } from '../gateway/auth/auth.gateway';
 import { AuthUseCase } from '../usecase/auth.usecase';
 
 describe('AccountEntity', () => {
   const errorGenerator: ErrorGenerator = { message: 'Erorr', status: 403 };
-  const auth = new AuthUseCase({} as any, errorGenerator);
+  const addUser = jest
+    .fn()
+    .mockReturnValue({ name: 'Kola', email: 'kola@hi.com' });
+  const auth = new AuthUseCase({ addUser } as any, errorGenerator);
 
   const checkUser = jest.spyOn(auth as any, 'checkUser');
   afterEach(() => checkUser.mockClear());
@@ -36,5 +41,17 @@ describe('AccountEntity', () => {
 
     expect((auth as any).checkUser).toBeCalledTimes(1);
     expect(user).toEqual(errorGenerator);
+  });
+
+  test('the correct data has been saved during the registration process', async () => {
+    checkUser.mockReturnValue(null);
+
+    const user = await auth.registration({
+      name: 'Kola',
+      email: 'kola@hi.com',
+    } as any);
+    expect((auth as any).checkUser).toBeCalledTimes(1);
+    expect(addUser).toBeCalledTimes(1);
+    expect(user).toEqual({ name: 'Kola', email: 'kola@hi.com' });
   });
 });
