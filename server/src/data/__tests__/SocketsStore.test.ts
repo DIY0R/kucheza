da8fac40e97a391c65cf3b57b4053921a9c334cf
diyor <sabirovdiyorkind@gmail.com>
2022-08-12 23:50:53 +0500

    write test for delete socket

--- server/src/data/__tests__/SocketsStore.test.ts
+++ server/src/data/__tests__/SocketsStore.test.ts
@@ -2,13 +2,18 @@ import net from 'net';
 import { SocketsStore } from '../SocketsStore';
 describe('check Sockets Store', () => {
   let socketsStore: any = {};
+  let sendMessage = { name: 'hello', id: 6 };
+  let id = 6;
   beforeEach(() => {
     socketsStore = new SocketsStore();
   });
   test('for adding sockets', () => {
-    let sendMessage = { name: 'hello', id: 6 };
-    let id = 6;
     socketsStore.addSocket(id, sendMessage);
     expect(socketsStore.sockets.get(id)).toEqual(sendMessage);
+  });
+
+  test('for delete socket', () => {
+    socketsStore.deleteSocket(id);
+    expect(socketsStore.sockets.get(id)).toBeUndefined();
   });
 });
